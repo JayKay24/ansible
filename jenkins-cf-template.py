55561a0aafec34bf3ffcca6bc063ce5feaf15906
James Kinyua <jameskinyua590@gmail.com>
2019-07-09 21:09:05 +0300

    chore: add new shell commands to user data field

--- jenkins-cf-template.py
+++ jenkins-cf-template.py
@@ -76,15 +76,7 @@ t.add_resource(ec2.SecurityGroup(
 ud = Base64(Join('\n', [ "#!/bin/bash",
 "yum install --enablerepo=epel -y git",
 "yum install --enablerepo=epel -y ansible",
-"sudo yum remove java-1.7.0-openjdk",
-"sudo yum install java-1.8.0-openjdk-devel",
-"curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
-"sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
-"sudo yum install jenkins",
-"sudo systemctl start jenkins",
-"sudo systemctl enable jenkins",
-"sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp"
-"sudo firewall-cmd --reload",
+AnsiblePullCmd
 ]))
 
 t.add_resource(Role(
